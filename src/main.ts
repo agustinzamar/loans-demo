e9996d7bf94e9f06ebec4f103e6e7c13857ed598
Agustin Zamar <agustinzamar33@gmail.com>
2026-02-17 14:01:34 -0300

    feat: add global validation pipe to enforce data integrity

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
   await app.listen(port);
